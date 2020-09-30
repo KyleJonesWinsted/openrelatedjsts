55965acb2f74e0426fbfd5459981d976c26e7a4b
Kyle Jones <kyle.jones@bergankdv.com>
2020-09-30 14:27:04 -0500

    added function to get current language, path, and folder

--- src/extension.ts
+++ src/extension.ts
@@ -1,27 +1,19 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
+
 import * as vscode from 'vscode';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "openrelatedjsts" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('openrelatedjsts.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from OpenRelatedJSTS!');
+	let openFile = vscode.commands.registerCommand('openrelatedjsts.openFile', () => {
+		vscode.window.showInformationMessage('ran the open file command');
+		console.log('document language:', vscode.window.activeTextEditor?.document.languageId);
+		console.log('document path:', vscode.window.activeTextEditor?.document.uri.fsPath);
+		let folders = vscode.workspace.workspaceFolders;
+		let folder;
+		if (folders) { folder = folders[0]; }
+		console.log('root dir:', folder?.uri.fsPath);
 	});
 
-	context.subscriptions.push(disposable);
+	context.subscriptions.push(openFile);
 }
 
-// this method is called when your extension is deactivated
 export function deactivate() {}
