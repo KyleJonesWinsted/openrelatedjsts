e90afa035e347459b2d8dd1ec7bb1859a943afcf
Kyle Jones <kyle.jones@bergankdv.com>
2020-10-05 15:57:31 -0500

    opens file if only one name matches

--- src/extension.ts
+++ src/extension.ts
@@ -3,17 +3,58 @@ import * as vscode from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
 
-	let openFile = vscode.commands.registerCommand('openrelatedjsts.openFile', () => {
+	let openFile = vscode.commands.registerCommand('openrelatedjsts.openFile', async () => {
 		vscode.window.showInformationMessage('ran the open file command');
-		console.log('document language:', vscode.window.activeTextEditor?.document.languageId);
-		console.log('document path:', vscode.window.activeTextEditor?.document.uri.fsPath);
-		let folders = vscode.workspace.workspaceFolders;
-		let folder;
-		if (folders) { folder = folders[0]; }
-		console.log('root dir:', folder?.uri.fsPath);
+		const activeTextEditor = vscode.window.activeTextEditor;
+		if (!activeTextEditor) { return; }
+		try {
+			const currentDocLang = getDocLanguage(activeTextEditor);
+			const currentDocPath = activeTextEditor.document.uri.fsPath;
+			const folders = vscode.workspace.workspaceFolders;
+			if (!folders || folders.length < 1) { return; }
+			const rootPath = folders[0].uri.fsPath;
+			
+			const files = await getMatchingFiles(currentDocLang, currentDocPath);
+			if (files.length === 1) {
+				const doc = await vscode.workspace.openTextDocument(files[0].path);
+				vscode.window.showTextDocument(doc);
+			}
+			
+		} catch (err) {
+			console.error('Error in openFile: ', err);
+			vscode.window.showErrorMessage('Error: ' + err);
+		}
 	});
 
 	context.subscriptions.push(openFile);
+}
+
+function getDocLanguage(activeTextEditor: vscode.TextEditor): LanguageId {
+	const currentDocLang = activeTextEditor.document.languageId;
+	switch (currentDocLang) {
+		case LanguageId.typescript:
+			return LanguageId.typescript;
+		case LanguageId.javascript:
+			return LanguageId.javascript;
+		default:
+			throw new Error("Current file is not typescript or javascript");
+	}
+}
+
+async function getMatchingFiles(currentDocLang: LanguageId, currentDocPath: string): Promise<vscode.Uri[]> {
+	return new Promise(resolve => {
+		let pathComponents = currentDocPath.split(/\/|\\/);
+		let searchString = '**/' + pathComponents[pathComponents.length - 1].slice(0, -2) + (currentDocLang === LanguageId.typescript ? 'js' : 'ts'); 
+		vscode.workspace.findFiles(searchString)
+			.then(files => {
+				resolve(files);
+			});
+	});
+}
+
+enum LanguageId {
+	typescript = 'typescript',
+	javascript = 'javascript',
 }
 
 export function deactivate() {}
